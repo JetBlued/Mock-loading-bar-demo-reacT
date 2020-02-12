d894ce420925a4d503fe894cb80803e2aca69ced
JetBlued <110317452+JetBlued@users.noreply.github.com>
2020-02-12 07:53:31 +0000

    Added screenshots.

--- ExampleContract.kt
+++ ExampleContract.kt
@@ -21,3 +21,28 @@ interface ExampleContract {
 
     @ContractAction
     fun addProtocol(
+            @InvokeParam(name="diagnosis") diagnosis: String,
+            @InvokeParam(name="complaints") complaints: String,
+            @InvokeParam(name="anamnesis") anamnesis: String,
+            @InvokeParam(name="treatment") treatment: String,
+            @InvokeParam(name="recommendations") recommendations: String,
+            @InvokeParam(name="prognosis") prognosis: String,
+            @InvokeParam(name="patient_key") patient_key: String
+    )
+
+    @ContractAction
+    fun addCommonInfo(
+            @InvokeParam(name="common_info") common_info: String,
+            @InvokeParam(name="patient_key") patient_key: String
+    )
+
+    @ContractAction
+    fun addTest(
+            @InvokeParam(name="name") name: String,
+            @InvokeParam(name="indicator_names") indicator_names: String,
+            @InvokeParam(name="results") results: String,
+            @InvokeParam(name="comments") comments: String,
+            @InvokeParam(name="patient_key") patient_key: String
+    )
+
+    @ContractAction
