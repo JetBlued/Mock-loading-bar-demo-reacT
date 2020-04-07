80805bf8487215314caa6545d81ecf5487acc310
JetBlued <110317452+JetBlued@users.noreply.github.com>
2020-04-07 20:38:04 +0000

    Add release instructions

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -2,4 +2,4 @@ package com.wavesplatform.we.app.example.contract.impl
 
 import com.wavesplatform.vst.contract.data.ContractCall
 import com.wavesplatform.vst.contract.mapping.Mapping
-import com.wavesplatform.vst.contract.spring.annota
+import com.wavesplatform.vst.contract.spring.annotation.ContractHandlerB
