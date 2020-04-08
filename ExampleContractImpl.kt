73838b40e32f54e12fd8488e54199e24e6a8a3d5
JetBlued <110317452+JetBlued@users.noreply.github.com>
2020-04-08 09:15:47 +0000

    Bump to v2.1.0

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -3,4 +3,4 @@ package com.wavesplatform.we.app.example.contract.impl
 import com.wavesplatform.vst.contract.data.ContractCall
 import com.wavesplatform.vst.contract.mapping.Mapping
 import com.wavesplatform.vst.contract.spring.annotation.ContractHandlerBean
-import com.wavesplatform.vst.c
+import com.wavesplatform.vst.contract.state.Co
