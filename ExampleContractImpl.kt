15db0c5e78d63cf9fc4ca4bfdd7f39c8b26011cf
JetBlued <110317452+JetBlued@users.noreply.github.com>
2020-04-13 13:49:57 +0000

    Debug messages

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -3,4 +3,5 @@ package com.wavesplatform.we.app.example.contract.impl
 import com.wavesplatform.vst.contract.data.ContractCall
 import com.wavesplatform.vst.contract.mapping.Mapping
 import com.wavesplatform.vst.contract.spring.annotation.ContractHandlerBean
-import com.wavesplatform.vst.contract.state.Co
+import com.wavesplatform.vst.contract.state.ContractState
+im
