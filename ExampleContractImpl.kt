4e4bfc824d49aa97dc39334c9df93db5e286fbb4
JetBlued <110317452+JetBlued@users.noreply.github.com>
2020-07-26 22:49:48 +0000

    chore(package): update eslint to version 3.2.0

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -25,4 +25,5 @@ class ExampleContractImpl(
 
     private val doctors: Mapping<Doctor> by state
     private val pharmacies: Mapping<Pharmacy> by state
-    private val stories: Mapping<History
+    private val stories: Mapping<History> by state
+ 
