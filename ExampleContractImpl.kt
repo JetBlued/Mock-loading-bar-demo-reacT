e20736e3d4da2e13359a5f4192f01f5b28dd5b2e
JetBlued <110317452+JetBlued@users.noreply.github.com>
2020-09-22 20:18:41 +0000

    Remove unused test

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -52,4 +52,6 @@ class ExampleContractImpl(
 
     override fun addDrugs(drug_name: String, information: String) {
         require(pharmacies.has(call.sender)) {
-            "ONLY_PHARMACIES_CAN_ADD_DRU
+            "ONLY_PHARMACIES_CAN_ADD_DRUGS"
+        }
+       
