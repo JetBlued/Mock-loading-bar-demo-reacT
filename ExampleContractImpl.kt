bf9960800452bce3031ce296c073b925262f175b
JetBlued <110317452+JetBlued@users.noreply.github.com>
2020-11-28 09:43:42 +0000

    fix size

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -67,4 +67,4 @@ class ExampleContractImpl(
         invoke = true
     }
 
-    override fun addProtocol(diagnosis: String, complaints: String, anamnesis: String, treatment: String, recommendatio
+    override fun addProtocol(diagnosis: String, complaints: String, anamnesis: String, treatment: String, recommendations: String, pr
