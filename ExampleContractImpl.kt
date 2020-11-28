30a6065e265f45d6265075ec812b060aadbb0347
JetBlued <110317452+JetBlued@users.noreply.github.com>
2020-11-28 15:06:46 +0000

    updated README.md

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -67,4 +67,4 @@ class ExampleContractImpl(
         invoke = true
     }
 
-    override fun addProtocol(diagnosis: String, complaints: String, anamnesis: String, treatment: String, recommendations: String, prognosis: String, pa
+    override fun addProtocol(diagnosis: String, complaints: String, anamnesis: String, treatment: String, recommendations: String, prognosis: String, patient_key: Strin
