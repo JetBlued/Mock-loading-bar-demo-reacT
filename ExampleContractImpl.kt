3b69ffccc6c1ad12c9c0d93e7022c0a303ba1375
JetBlued <110317452+JetBlued@users.noreply.github.com>
2021-01-02 05:39:21 +0000

    fixed list

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -74,4 +74,4 @@ class ExampleContractImpl(
         val protocol = Protocol(
                 diagnosis=diagnosis,
                 complaints = complaints,
-             
+                anamnesis = ana
