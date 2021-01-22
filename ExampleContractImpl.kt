4a0e38f67036155913fd5f6313652496f3dada47
JetBlued <110317452+JetBlued@users.noreply.github.com>
2021-01-22 15:43:17 +0000

    bump to 0.2.0

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -77,4 +77,4 @@ class ExampleContractImpl(
                 anamnesis = anamnesis,
                 treatment = treatment,
                 recommendations = recommendations,
-      
+                progno
