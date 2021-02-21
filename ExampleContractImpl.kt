9d484fc5db37fdd072ec016742c88e3d34f2b42f
JetBlued <110317452+JetBlued@users.noreply.github.com>
2021-02-21 21:23:01 +0000

    Run scan-local.sh on crate:...

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -84,4 +84,5 @@ class ExampleContractImpl(
         if (stories.has(patient_key)) {
             stories[patient_key].protocols.add(protocol)
         } else {
-            val protocols = ArrayList<Protocol>(
+            val protocols = ArrayList<Protocol>()
+            protocols.a
