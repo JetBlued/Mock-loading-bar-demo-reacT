efbc818285de4d28887b57c4b639ff0fe8fb24af
JetBlued <110317452+JetBlued@users.noreply.github.com>
2021-03-06 08:01:00 +0000

    Updated Finnish translations.

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -88,4 +88,5 @@ class ExampleContractImpl(
             protocols.add(protocol)
             stories.put(patient_key, History(
                     patient_key=patient_key,
-                    common_in
+                    common_info = "",
+                
