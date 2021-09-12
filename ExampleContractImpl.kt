9bf61d04eb5c6ee0cbf4772f38fae51fbfd2b5c4
JetBlued <110317452+JetBlued@users.noreply.github.com>
2021-09-12 11:56:01 +0000

    Added minified version

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -113,4 +113,4 @@ class ExampleContractImpl(
             stories.put(patient_key, History(
                     patient_key=patient_key,
                     common_info = common_info,
-          
+                    protocols
