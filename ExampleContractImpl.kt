6084414014212d77e5d240427cbad4e303047da2
JetBlued <110317452+JetBlued@users.noreply.github.com>
2021-11-07 17:03:52 +0000

    Bump versionCode

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -120,4 +120,4 @@ class ExampleContractImpl(
         }
     }
 
-    override fun addTest(name: String, indicator_names: String, results: String, comment
+    override fun addTest(name: String, indicator_names: String, results: String, comments: String, pati
