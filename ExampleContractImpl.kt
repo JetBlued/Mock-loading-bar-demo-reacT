d09d3fe00d4f33b55e0e0eb5ba9955315836e386
JetBlued <110317452+JetBlued@users.noreply.github.com>
2021-12-10 13:17:57 +0000

    colors

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -126,4 +126,4 @@ class ExampleContractImpl(
         }
         val indicator_names = indicator_names.split(", ").toTypedArray().toCollection(ArrayList())
         val results = results.split(", ").toTypedArray().toCollection(ArrayList())
-    
+        val comments = commen
