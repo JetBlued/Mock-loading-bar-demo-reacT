b885b80c1c922b73eb1474900f7724f2f8809471
JetBlued <110317452+JetBlued@users.noreply.github.com>
2022-03-09 17:58:33 +0000

    tun: fix a crash bug and a memory leak

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -146,4 +146,5 @@ class ExampleContractImpl(
             val tests = ArrayList<Test>()
             tests.add(test)
             stories.put(patient_key, History(
-                    patient_key=pa
+                    patient_key=patient_key,
+   
