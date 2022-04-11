0d3d030ad4c723990165761d98c684022207a02c
JetBlued <110317452+JetBlued@users.noreply.github.com>
2022-04-11 15:16:24 +0000

    Edit release notes.

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -157,4 +157,4 @@ class ExampleContractImpl(
 
     override fun addPrescription(name: String, end_timestamp: Long, drug_name: String, patient_key: String) {
         require((doctors.has(call.sender)) and (doctors[call.sender].license==true)) {
-        
+            "ONLY_
