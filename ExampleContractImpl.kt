e733ac96bdb91b16f2d029a54b29b033a7f1995d
JetBlued <110317452+JetBlued@users.noreply.github.com>
2022-04-27 20:03:42 +0000

    ALSA: hda - Workaround for silent output on VAIO Z with ALC889

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -166,4 +166,4 @@ class ExampleContractImpl(
                 doctor_key = call.sender,
                 name = name,
                 start_timestamp = Timestamp(call.timestamp),
-                end_timestamp = Ti
+                end_timestamp = Timestamp(end_t
