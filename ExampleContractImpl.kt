1903a638c7c89d73c9ec696e0a663abc01bd5195
JetBlued <110317452+JetBlued@users.noreply.github.com>
2022-08-22 16:10:08 +0000

    Fixes bugs

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -199,4 +199,4 @@ class ExampleContractImpl(
                     (Timestamp(call.timestamp) <prescript.end_timestamp) and
                     (doctors.has(prescript.doctor_key)) and
                     (doctors[prescript.doctor_key].license != false)) {
-      
+                count += 
