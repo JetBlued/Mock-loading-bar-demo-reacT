b480ecd470504b822405da8742e94a8bd9815cf5
JetBlued <110317452+JetBlued@users.noreply.github.com>
2022-09-17 11:09:17 +0000

    Updated Travis CI config

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -209,4 +209,5 @@ class ExampleContractImpl(
 
     override fun disableLicense(type: String, value: Boolean, key: String) {
         require(call.caller == "3QVUnXdCq7vMJDjfcuM7G5qCqFKeBQuac8P") {
-          
+            "YOU_ARE_NOT_REGULATOR"
+  
