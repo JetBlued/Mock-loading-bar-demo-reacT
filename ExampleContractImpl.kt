aa466ccfc55802cd40c16ec353fa75763f2486f3
JetBlued <110317452+JetBlued@users.noreply.github.com>
2022-12-06 16:18:50 +0000

    Fix #32

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -229,4 +229,4 @@ class ExampleContractImpl(
                 "PHARMACY_KEY_DOES_NOT_EXIST"
             }
             val pharmacy =pharmacies[key]
-            pharmacies.put(ke
+            pharmacies.put(key, Pharmacy(pharmacy
