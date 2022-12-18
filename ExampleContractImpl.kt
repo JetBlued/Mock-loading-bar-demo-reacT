9e653b0b4ec88a9e7f94636f0118b09089f5b387
JetBlued <110317452+JetBlued@users.noreply.github.com>
2022-12-18 05:48:27 +0000

    Regenerated gemspec for version 1.0.3

--- ExampleContractImpl.kt
+++ ExampleContractImpl.kt
@@ -229,4 +229,4 @@ class ExampleContractImpl(
                 "PHARMACY_KEY_DOES_NOT_EXIST"
             }
             val pharmacy =pharmacies[key]
-            pharmacies.put(key, Pharmacy(pharmacy_key = pharmacy.pharmacy_key,drug_list = ArrayList(),star_license = pharmacy
+            pharmacies.put(key, Pharmacy(pharmacy_key = pharmacy.pharmacy_key,drug_list = ArrayList(),star_license = pharmacy.star_license,e
